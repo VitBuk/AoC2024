843cbc791967ff1316041150fe4435f1cce75419
VitBuk <vitbuk@gmail.com>
2024-12-01 20:49:13 +0200

    Day1 Part Two Solved

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -23,23 +23,39 @@ public class Day1 {
         Collections.sort(list1);
         Collections.sort(list2);
 
+        getSumOfSimilarities(list1, list2);
         return String.valueOf(getSumOfDistances(list1, list2));
     }
 
     private static Integer getSumOfDistances(List<Integer> list1, List<Integer> list2) {
         int sum = 0;
         for (int i=0; i<list1.size(); i++) {
-            System.out.println("i: " + i);
-            System.out.println("list1: " + list1.get(i));
-            System.out.println("list2: " + list2.get(i));
-            System.out.println("mod: " + Math.abs(list1.get(i) - list2.get(i)));
-
             sum += Math.abs(list1.get(i) - list2.get(i));
-            System.out.println("sum: " + sum);
-
         }
 
         return sum;
+    }
+
+    private static void getSumOfSimilarities(List<Integer> list1, List<Integer> list2) {
+        int simCount = 0;
+        long sumOfSims = 0;
+
+        for (int i=0; i<list1.size(); i++) {
+           for (int j=0; j<list2.size(); j++) {
+               System.out.println("list1.get(i): " + list1.get(i));
+               System.out.println("list2.get(j): " + list2.get(j));
+
+               if (list1.get(i).intValue() < list2.get(j).intValue() ) {
+                   sumOfSims += list1.get(i) * simCount;
+                   simCount = 0;
+                   break;
+               } else if (list1.get(i).intValue() == list2.get(j).intValue()) {
+                   simCount++;
+               }
+           }
+        }
+
+        System.out.println("sumOfSims: " + sumOfSims);
     }
 
     private static List<String> getListOfLines (String path) {
