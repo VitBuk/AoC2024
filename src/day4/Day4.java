bbe9f285058eade73de6acd2264a4bd6f2594712
VitBuk <vitbuk@gmail.com>
2024-12-04 18:40:49 +0200

    some useful methods now part of StringUtils

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -1,8 +1,8 @@
 package day4;
 
 import utils.FileUtils;
+import utils.StringUtils;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -48,99 +48,33 @@ public class Day4 {
     }
 
     private static Integer findAllUpToBottom(String input) {
-        return findALlLeftToRight(columnsToRows(input));
+        return findALlLeftToRight(StringUtils.columnsToRows(input));
     }
 
     private static Integer findAllBottomToUp(String input) {
-        String newString = columnsToRows(input);
+        String newString = StringUtils.columnsToRows(input);
         String reversedNewString = new StringBuilder(newString).reverse().toString();
         return findALlLeftToRight(reversedNewString);
     }
 
     private static Integer findAllNWDiagonals(String input) {
-        return findALlLeftToRight(NWDiagonalsToRows(input));
+        return findALlLeftToRight(StringUtils.NWDiagonalsToRows(input));
     }
 
     private static Integer findAllSWDiagonals(String input) {
         String reversed = new StringBuilder(input).reverse().toString();
-        return findALlLeftToRight(NWDiagonalsToRows(reversed));
+        return findALlLeftToRight(StringUtils.NWDiagonalsToRows(reversed));
     }
 
     private static Integer findALlNEDiagonals(String input) {
-        String allRowsReversed = allRowsReversed(input);
+        String allRowsReversed = StringUtils.allRowsReversed(input);
         return findAllNWDiagonals(allRowsReversed);
     }
 
     private static Integer findAllSEDiagonals(String input) {
-        String allRowsReversed = allRowsReversed(input);
+        String allRowsReversed = StringUtils.allRowsReversed(input);
         String reverseAllRowsReversed = new StringBuilder(allRowsReversed).reverse().toString();
         return findAllNWDiagonals(reverseAllRowsReversed);
-    }
-
-    private static String allRowsReversed (String input) {
-        return Arrays.stream(input.split("\\R"))
-                .map(line -> new StringBuilder(line).reverse().toString())
-                .collect(Collectors.joining("\n"));
-    }
-
-    private static String NWDiagonalsToRows(String input) {
-        String[] lines = input.split("\n");
-
-        int length = lines.length;
-
-        int diagonalsAmount = 2 * length - 1;
-
-        StringBuilder[] diagonals = new StringBuilder[diagonalsAmount];
-        for (int i = 0; i < diagonalsAmount; i++) {
-            diagonals[i] = new StringBuilder();
-        }
-
-        for (int k = 0; k < diagonalsAmount; k++) {
-            int startRow = Math.max(0, k - length + 1);
-            int endRow = Math.min(length - 1, k);
-
-            for (int i = startRow; i <= endRow; i++) {
-                int j = k - i;
-                char ch = lines[i].charAt(j);
-                diagonals[k].insert(0, ch);
-            }
-        }
-
-        StringBuilder newString = new StringBuilder();
-        for (int i = 0; i < diagonalsAmount; i++) {
-            newString.append(diagonals[i].toString());
-            if (i < diagonalsAmount - 1) {
-                newString.append('\n');
-            }
-        }
-
-        return newString.toString();
-    }
-
-    private static String columnsToRows(String input) {
-        String[] lines = input.split("\n");
-
-        StringBuilder[] newLines = new StringBuilder[lines[0].length()];
-
-        for (int i=0; i < lines[0].length(); i++) {
-            newLines[i] = new StringBuilder();
-        }
-
-        for (String line : lines) {
-            for (int column = 0; column < lines[0].length(); column++) {
-                newLines[column].append(line.charAt(column));
-            }
-        }
-
-        StringBuilder newString = new StringBuilder();
-        for (int i=0; i<lines[0].length(); i++) {
-            newString.append(newLines[i].toString());
-            if ( i < lines[0].length() - 1) {
-                newString.append("\n");
-            }
-        }
-
-        return newString.toString();
     }
 
     private static Integer findAllMASCrosses(String input) {
--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringUtils {
     public static List<String> splitBySpaces(String text) {
@@ -9,4 +10,71 @@ public class StringUtils {
         String[] tokens = text.split(" +");
         return Arrays.asList(tokens);
     }
+
+    public static String columnsToRows(String input) {
+        String[] lines = input.split("\n");
+
+        StringBuilder[] newLines = new StringBuilder[lines[0].length()];
+
+        for (int i=0; i < lines[0].length(); i++) {
+            newLines[i] = new StringBuilder();
+        }
+
+        for (String line : lines) {
+            for (int column = 0; column < lines[0].length(); column++) {
+                newLines[column].append(line.charAt(column));
+            }
+        }
+
+        StringBuilder newString = new StringBuilder();
+        for (int i=0; i<lines[0].length(); i++) {
+            newString.append(newLines[i].toString());
+            if ( i < lines[0].length() - 1) {
+                newString.append("\n");
+            }
+        }
+
+        return newString.toString();
+    }
+
+    public static String allRowsReversed (String input) {
+        return Arrays.stream(input.split("\\R"))
+                .map(line -> new StringBuilder(line).reverse().toString())
+                .collect(Collectors.joining("\n"));
+    }
+
+    public static String NWDiagonalsToRows(String input) {
+        String[] lines = input.split("\n");
+
+        int length = lines.length;
+
+        int diagonalsAmount = 2 * length - 1;
+
+        StringBuilder[] diagonals = new StringBuilder[diagonalsAmount];
+        for (int i = 0; i < diagonalsAmount; i++) {
+            diagonals[i] = new StringBuilder();
+        }
+
+        for (int k = 0; k < diagonalsAmount; k++) {
+            int startRow = Math.max(0, k - length + 1);
+            int endRow = Math.min(length - 1, k);
+
+            for (int i = startRow; i <= endRow; i++) {
+                int j = k - i;
+                char ch = lines[i].charAt(j);
+                diagonals[k].insert(0, ch);
+            }
+        }
+
+        StringBuilder newString = new StringBuilder();
+        for (int i = 0; i < diagonalsAmount; i++) {
+            newString.append(diagonals[i].toString());
+            if (i < diagonalsAmount - 1) {
+                newString.append('\n');
+            }
+        }
+
+        return newString.toString();
+    }
+
 }
