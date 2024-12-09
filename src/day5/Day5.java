36ad78666ca0718b22690ebfcde3628ddeaa4306
VitBuk <vitbuk@gmail.com>
2024-12-09 18:15:51 +0200

    day5 scratches

--- src/day5/Day5.java
+++ src/day5/Day5.java
@@ -1,11 +1,15 @@
 package day5;
 
-import utils.FileUtils;
+import java.util.List;
 
 public class Day5 {
 
     private static final String path = "src\\day5\\input.txt";
     private static final String rulesPath = "src\\day5\\rules.txt";
+    private static List<Rule> rules;
     public static void getAnswer(){
+    }
+
+    private static void setRules(String input) {
     }
 }
